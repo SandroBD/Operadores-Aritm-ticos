32adb1fc3121fac193d530599ac409a3ebea3a42
Sandro Marques <silva08ma@gmail.com>
2024-10-08 20:43:04 -0300

    Dez (10) exercícios com operadores aritméticos

--- Desafio 11.py
+++ Desafio 11.py
@@ -1,7 +1,7 @@
-a=int(input('Informe a altura da parede: '))
-l=int(input('Informe a largura da parede: '))
+a=float(input('Informe a altura da parede: '))
+l=float(input('Informe a largura da parede: '))
 area = a * l
 tinta = area / 2
-print('A area da parede é {} e será necessário {} litros de tinta para pintá-la'.format(area, tinta))
+print('A area da parede é {}m² e será necessário {} litros de tinta para pintá-la'.format(area, tinta))
 
 
--- Desafio 12.py
+++ Desafio 12.py
@@ -1,2 +1,2 @@
-n=float(input('Informe o valor: '))
-print('O produto que vale R${}, com 5% de desconto, ficará R${}'.format(n, n - n * 0.05))
+n=float(input('Informe o valor: R$'))
+print('O produto que vale R${:.2f}, com 5% de desconto, ficará R${:.2f}'.format(n, n - n * 0.05))
